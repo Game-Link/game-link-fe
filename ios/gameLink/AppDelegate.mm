f736ca3e6f408f3816bdb1d3dc222e3d59fb6e71
kd02109 <kd02109@gmail.com>
2024-11-19 19:32:34 +0900

    bug: ios firebase error 해결

--- ios/gameLink/AppDelegate.mm
+++ ios/gameLink/AppDelegate.mm
@@ -2,9 +2,7 @@
 #import <React/RCTBundleURLProvider.h>
 #import <RNKakaoLogins.h>
 #import <NaverThirdPartyLogin/NaverThirdPartyLoginConnection.h>
-#import <Firebase.h>;
-#import <UserNotifications/UserNotifications.h>
-#import <RNCPushNotificationIOS.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
@@ -24,49 +22,21 @@
   return NO; // 처리되지 않은 URL은 NO 반환
 }
 
-// notifications ios
-// Required for the register event.
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
- [RNCPushNotificationIOS didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-// Required for the notification event. You must call the completion handler after handling the remote notification.
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
-{
-  [RNCPushNotificationIOS didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
-}
-// Required for the registrationError event.
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
-{
- [RNCPushNotificationIOS didFailToRegisterForRemoteNotificationsWithError:error];
-}
-// Required for localNotification event
-- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-didReceiveNotificationResponse:(UNNotificationResponse *)response
-         withCompletionHandler:(void (^)(void))completionHandler
-{
-  [RNCPushNotificationIOS didReceiveNotificationResponse:response];
-}
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  #ifdef FB_SONARKIT_ENABLED
+  InitializeFlipper(application);
+  #endif
+
+  // Firebase 초기화
+  if ([FIRApp defaultApp] == nil) { // 추가된 부분
+    [FIRApp configure];
+  }
+
   self.moduleName = @"gameLink";
   self.initialProps = @{};
 
-  if ([FIRApp defaultApp] == nil) { // 추가 (line:35)
-  [FIRApp configure];
-  }
-
-  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-  center.delegate = self;
-
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
-
--(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
-{
-  completionHandler(UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge);
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
