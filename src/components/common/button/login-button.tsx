828a80dad71d69e4097f5e7379ba579219b3950e
kd02109 <kd02109@gmail.com>
2025-03-02 19:02:35 +0900

    feat: naver login 버튼

--- src/components/common/button/login-button.tsx
+++ src/components/common/button/login-button.tsx
@@ -1,33 +1,48 @@
-import {Pressable, PressableProps, StyleSheet, StyleProp} from 'react-native';
+import {
+  Pressable,
+  PressableProps,
+  StyleSheet,
+  StyleProp,
+  TextStyle,
+  Image,
+} from 'react-native';
 import React from 'react';
 import {Text} from 'react-native-paper';
+import {DEFAULT_STYLES} from '@src/util';
 
 type Props = {
   title: string;
   style?: StyleProp<any>;
   backgroundColor?: string;
+  logo?: any;
+  textStyle?: StyleProp<TextStyle>;
 } & Omit<PressableProps, 'style'>;
 
 export default function LoginButton({
   title,
   backgroundColor,
   style,
+  textStyle,
+  logo,
   ...props
 }: Props) {
   return (
     <Pressable style={[styles.pressable, {backgroundColor}, style]} {...props}>
-      <Text style={styles.text}>{title}</Text>
+      {logo && <Image source={logo} style={styles.logo} />}
+      <Text style={[styles.text, textStyle]}>{title}</Text>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   pressable: {
-    width: 300,
+    width: 280,
     paddingVertical: 16,
     borderRadius: 8,
-    borderWidth: 1,
-    borderColor: '#4a4a4a', // assuming "border-darkgray" refers to a dark gray color
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     textAlign: 'center',
@@ -35,5 +50,10 @@ const styles = StyleSheet.create({
     color: '#4a4a4a', // assuming "text-darkgray" refers to dark gray
     fontSize: 16,
     fontFamily: 'PretendardExtraBold', // assuming you're using a custom font
+  },
+  logo: {
+    width: DEFAULT_STYLES.width['8'],
+    height: DEFAULT_STYLES.width['8'],
+    marginRight: DEFAULT_STYLES.size['8'],
   },
 });
