3f5aa2f917155ae2aa6d3b71d6728640f7a791b5
kd02109 <kd02109@gmail.com>
2025-03-04 15:56:05 +0900

    feat: 시작화면 bottom sheet 도입

--- src/page/sign-up/index.tsx
+++ src/page/sign-up/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {
+  BottomSheetComponent,
   GoogleLoginService,
   KakaoWebview,
+  LoginButton,
   NaverLoginService,
 } from '@src/components';
 import {useKakaoOauthLoginMutation} from '@api';
@@ -11,13 +13,22 @@ import {
   responsiveScreenWidth,
 } from 'react-native-responsive-dimensions';
 import Logo from '@src/assets/appstore.png';
+import {DEFAULT_STYLES} from '@src/util';
+import {useBottomSheet} from '@src/hooks';
+import {BottomSheetModal} from '@gorhom/bottom-sheet';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamList} from '../navigation';
 
-export default function SignUp() {
+type SignUpProps = StackNavigationProp<RootStackParamList, 'SignUp'>;
+export default function SignUp({navigate}: SignUpProps) {
   const mutation = useKakaoOauthLoginMutation();
 
   const onKakaoLogin = async (token: string) => {
     mutation.mutate(token);
   };
+
+  const {bottomSheetRef, handleSheetChanges, handlePresentModalPress} =
+    useBottomSheet();
 
   return (
     <View style={styles.container}>
@@ -27,12 +38,59 @@ export default function SignUp() {
         accessibilityLabel="Game Link Logo"
       />
       <Text style={styles.title}>GameLink</Text>
-      <NaverLoginService style={styles.gap} />
-      {/* <KakaoWebview onLogin={onKakaoLogin} /> */}
-      <GoogleLoginService />
+      <LoginButton
+        title="GameLink 시작하기"
+        backgroundColor={DEFAULT_STYLES.color.main}
+        style={[styles.gap]}
+        textStyle={styles.loginTtext}
+        onPress={handlePresentModalPress}
+      />
+      <LoginButton
+        title="GameLink 회원가입"
+        backgroundColor={DEFAULT_STYLES.color.lime}
+        style={[styles.gap]}
+        textStyle={styles.signUpText}
+        onPress={handlePresentModalPress}
+      />
+
+      <BottomSheetLogin
+        ref={bottomSheetRef}
+        handleSheetChanges={handleSheetChanges}
+      />
     </View>
   );
 }
+
+type BottomSheetLoginProps = {
+  handleSheetChanges: (index: number) => void;
+  points?: number;
+};
+
+const BottomSheetLogin = forwardRef<BottomSheetModal, BottomSheetLoginProps>(
+  ({handleSheetChanges, points = 40}, ref) => {
+    return (
+      <BottomSheetComponent
+        ref={ref}
+        points={points}
+        handleSheetChanges={handleSheetChanges}>
+        <View style={bottomSheetStyles.container}>
+          <NaverLoginService style={styles.gap} />
+          {/* <KakaoWebview onLogin={onKakaoLogin} /> */}
+          <GoogleLoginService />
+        </View>
+      </BottomSheetComponent>
+    );
+  },
+);
+
+const bottomSheetStyles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -55,5 +113,18 @@ const styles = StyleSheet.create({
   },
   gap: {
     marginBottom: 8,
+  },
+  loginTtext: {
+    color: DEFAULT_STYLES.color.white,
+    fontWeight: 'bold',
+    fontSize: DEFAULT_STYLES.fontSize.large,
+  },
+  signUpText: {
+    color: DEFAULT_STYLES.color.black,
+    fontWeight: 'bold',
+    fontSize: DEFAULT_STYLES.fontSize.large,
+  },
+  button: {
+    borderWidth: 1,
   },
 });
